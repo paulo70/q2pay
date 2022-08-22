f4fbca8473d838409e41a4dceb6e6e8448e06288
Paulo Costa <paulofrontend@gmail.com>
2022-08-22 18:02:21 -0300

    adjust component

---
+++ src/components/Results/index.js
@@ -0,0 +1,10 @@
+import { BoxResults } from "./style"
+const Results = ({ children }) => {
+  return (
+    <BoxResults>
+      <h3>{children}</h3>
+    </BoxResults>
+  )
+}
+
+export default Results
---
+++ src/components/Results/style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const BoxResults = styled.div`
+  display: flex;
+  justify-content: left;
+  
+  text-transform: uppercase;
+  text-align: left;
+  color: #0054FF;
+  font-size: 20px;
+
+  &{
+    h3{
+      margin-top: 20px;
+      margin-left: 20px;
+    }
+  }
+`
