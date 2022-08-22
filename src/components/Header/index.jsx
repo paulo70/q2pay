42ee6f2dfb6358ed1ed423fcd402f60eeba9043c
Paulo Costa <paulofrontend@gmail.com>
2022-08-22 14:30:13 -0300

    adding adjust header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import { ContentHeader, Brand, Span } from "./style"
+
+const Header = () => {
+  return (
+    <ContentHeader>
+      <Brand />
+      <Span>
+        filmes
+      </Span>
+    </ContentHeader>
+  )
+}
+
+export default Header
---
+++ src/components/Header/style.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+import { ReactComponent as Icon } from '../../assets/images/Quero2Pay_logo.svg'
+
+export const ContentHeader = styled.div`
+  width: 100%;
+  height: 100px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom:0vh;
+  z-index: 999px;
+  box-shadow: 0px 0px 6px 0px rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  
+  background-color: #0054FF;
+
+ `
+
+export const Brand = styled(Icon)`
+  width: 150px;
+  height: 50px;
+`
+
+export const Span = styled.span`
+  color: #fff;
+  font-size: 20px;
+  margin-top: 5px;
+  text-transform: uppercase;
+`
